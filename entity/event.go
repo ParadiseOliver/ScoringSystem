7cffe02825641ddc5bd802dd000f8ad733b380fc
Ollie Paradise <ollieparadise@yahoo.co.uk>
2023-01-22 13:38:34 +0000

    Further extended event model.

--- entity/event.go
+++ entity/event.go
@@ -1,11 +1,29 @@
 package entity
 
 type Event struct {
-	Id          string   `json:"id"`
-	Name        string   `json:"name"`
-	StartDate   string   `json:"startdate"`
-	EndDate     string   `json:"enddate"`
-	Disciplines []string `json:"disciplines"`
-	Categories  []string `json:"categories"`
-	Agegroups   []string `json:"agegroups"`
+	Id             string          `json:"id"`
+	Name           string          `json:"name"`
+	StartDate      string          `json:"startdate"`
+	EndDate        string          `json:"enddate"`
+	EntryDeadline  string          `json:"entry_deadline"`
+	Disciplines    []string        `json:"disciplines"`
+	Categories     []string        `json:"categories"`
+	Agegroups      []Agegroup      `json:"agegroups"`
+	Genders        []string        `json:"genders"`
+	CategoryGroups []CategoryGroup `json:"category_groups"`
+}
+
+type Agegroup struct {
+	Id           string `json:"id"`
+	MinAge       int    `json:"min_age"`
+	MaxAge       int    `json:"max_age"`
+	CategoryName string `json:"category_name"`
+}
+
+type CategoryGroup struct {
+	Id           string `json:"id"`
+	DisciplineId string `json:"discipline"`
+	CategoryId   string `json:"category"`
+	AgegroupId   string `json:"agegroup"`
+	GenderId     string `json:"gender"`
 }
