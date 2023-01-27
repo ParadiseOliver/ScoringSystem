06ebba44b66c75ae384c011b8e5c2d807dc0ef33
Ollie Paradise <ollieparadise@yahoo.co.uk>
2023-01-27 14:15:36 +0000

    Tidied main.

--- main.go
+++ main.go
@@ -30,49 +30,6 @@ func setupLogOutput() {
 	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 }
 
-/* func resultsByAthleteId(c *gin.Context) {
-	eventId := c.Param("eventId")
-	eventId = "results_" + eventId
-	athleteId := c.Param("athleteId")
-	log.Print(eventId, athleteId)
-	results, err := getResultsByAthleteId(eventId, athleteId)
-
-	if err != nil {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Result not found"})
-		return
-	}
-	c.IndentedJSON(http.StatusOK, results)
-} */
-
-/* func getResultsByAthleteId(eventId string, athleteId string) ([]entity.Result, error) {
-
-	var results []entity.Result
-
-	db, err := config.Connectdb()
-
-	if err != nil {
-		panic(err)
-	}
-
-	sql := "SELECT id, athlete_id, club_id, agegroup, category, score FROM " + eventId + " WHERE athlete_id = '" + athleteId + "'"
-	res, err := db.Query(sql)
-
-	if err != nil {
-		return nil, errors.New("event not found")
-	}
-
-	for res.Next() {
-		var result entity.Result
-		if err = res.Scan(&result.Id, &result.Athlete, &result.Club, &result.Category, &result.Score); err != nil {
-			panic(err)
-		}
-
-		results = append(results, result)
-	}
-
-	return results, nil
-} */
-
 func main() {
 
 	setupLogOutput()
