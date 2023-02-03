731722f76c4c5ba4b38a4d9d85c3bce27e076e4a
Ollie Paradise <ollieparadise@yahoo.co.uk>
2023-02-03 17:14:53 +0000

    Added new controller to begin separation/categorisation of endpoints.

---
+++ controllers/globals-controller.go
@@ -0,0 +1,142 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/ParadiseOliver/ScoringSystem/entity"
+	"github.com/ParadiseOliver/ScoringSystem/usecases"
+	"github.com/gin-gonic/gin"
+)
+
+type GlobalController interface {
+	AllDisciplines(ctx *gin.Context)
+	AddDiscipline(ctx *gin.Context)
+	DelDiscipline(ctx *gin.Context)
+	AllCategories(ctx *gin.Context)
+	AllAgeGroups(ctx *gin.Context)
+	AllGenders(ctx *gin.Context)
+	AllCategoryGroups(ctx *gin.Context)
+}
+
+type globalController struct {
+	service usecases.EventService
+}
+
+func NewGlobalController(service usecases.EventService) GlobalController {
+	//validate := validator.New()
+	//validate.RegisterValidation("is-after", validators.ValidateIsAfter)
+	return &globalController{
+		service: service,
+	}
+}
+
+func (c *globalController) AllDisciplines(ctx *gin.Context) {
+	disciplines, err := c.service.AllDisciplines()
+
+	if err != nil {
+		log.Printf("Failed to get disciplines: %v", err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+	wrapped := struct {
+		Fields []entity.Discipline `json:"disciplines"`
+	}{
+		disciplines,
+	}
+	ctx.JSON(http.StatusOK, wrapped)
+}
+
+func (c *globalController) AddDiscipline(ctx *gin.Context) {
+	var discipline *entity.Discipline
+	err := ctx.ShouldBindJSON(&discipline)
+	if err != nil {
+		log.Printf("Failed to add discipline: %v", err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+
+	discipline, err = c.service.AddDiscipline(discipline)
+	if err != nil {
+		log.Printf("Failed to add discipline: %v", err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+	ctx.JSON(http.StatusOK, discipline)
+}
+
+func (c *globalController) DelDiscipline(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	err := c.service.DelDiscipline(id)
+	if err != nil {
+		log.Printf("Failed to delete discipline: %v", err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+	ctx.Status(http.StatusOK)
+}
+
+func (c *globalController) AllCategories(ctx *gin.Context) {
+	categories, err := c.service.AllCategories()
+
+	if err != nil {
+		log.Printf("Failed to get categories: %v", err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+	wrapped := struct {
+		Fields []entity.Category `json:"categories"`
+	}{
+		categories,
+	}
+	ctx.JSON(http.StatusOK, wrapped)
+}
+
+func (c *globalController) AllAgeGroups(ctx *gin.Context) {
+	ageGroups, err := c.service.AllAgeGroups()
+
+	if err != nil {
+		log.Printf("Failed to get age groups: %v", err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+	wrapped := struct {
+		Fields []entity.AgeGroup `json:"agegroups"`
+	}{
+		ageGroups,
+	}
+	ctx.JSON(http.StatusOK, wrapped)
+}
+
+func (c *globalController) AllGenders(ctx *gin.Context) {
+	genders, err := c.service.AllGenders()
+
+	if err != nil {
+		log.Printf("Failed to get genders: %v", err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+	wrapped := struct {
+		Fields []entity.Gender `json:"genders"`
+	}{
+		genders,
+	}
+	ctx.JSON(http.StatusOK, wrapped)
+}
+
+func (c *globalController) AllCategoryGroups(ctx *gin.Context) {
+	categoryGroups, err := c.service.AllCategoryGroups()
+
+	if err != nil {
+		log.Printf("Failed to get category groups: %v", err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+	wrapped := struct {
+		Fields []entity.CategoryGroup `json:"category_groups"`
+	}{
+		categoryGroups,
+	}
+	ctx.JSON(http.StatusOK, wrapped)
+}
