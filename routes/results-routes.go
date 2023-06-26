fa179f3eb82a2a796aa14b2bac24819ab72b4adc
Ollie Paradise <ollieparadise@yahoo.co.uk>
2023-06-26 12:54:12 +0100

    Added ability to submit score to db

--- routes/results-routes.go
+++ routes/results-routes.go
@@ -6,10 +6,12 @@ type ResultsController interface {
 	ResultByResultId(ctx *gin.Context)
 	AllResultsByEventId(ctx *gin.Context)
 	ResultsByAthleteId(ctx *gin.Context)
+	ScoreAthlete(ctx *gin.Context)
 }
 
 func Results(results *gin.RouterGroup, resultController ResultsController) {
 	results.GET("/:resultId", resultController.ResultByResultId)
 	results.GET("/event/:eventId", resultController.AllResultsByEventId)
 	results.GET("/athlete/:athleteId", resultController.ResultsByAthleteId)
+	results.POST("/score/:eventId/:athleteId", resultController.ScoreAthlete)
 }
--- usecases/results-service.go
+++ usecases/results-service.go
@@ -7,6 +7,7 @@ type ResultsRepository interface {
 	ResultByResultId(id string) (*entity.Result, error)
 	ResultsByAthleteId(id string) ([]entity.Result, error)
 	UserByUserId(id string) (*entity.User, error)
+	ScoreAthlete(eventId, athleteId int, score *entity.TriScore) (*entity.Result, error)
 }
 
 type resultsService struct {
@@ -33,4 +34,8 @@ func (service resultsService) ResultsByAthleteId(id string) ([]entity.Result, er
 
 func (service resultsService) UserByUserId(id string) (*entity.User, error) {
 	return service.repo.UserByUserId(id)
+}
+
+func (service resultsService) ScoreAthlete(eventId, athleteId int, score *entity.TriScore) (*entity.Result, error) {
+	return service.repo.ScoreAthlete(eventId, athleteId, score)
 }
